5ee2b6fcbf86fe62e34d0e98d9eabb830c8ff1b4
Shane Starr <shane.starr@gm.com>
2019-03-13 15:05:42 -0500

    Adds classes.json with more config data, makes script more modular

--- rollcharacter.py
+++ rollcharacter.py
@@ -1,14 +1,15 @@
 import dice
 import texttable as tt
 import json
+import math
 
 with open('data/modifiers.json') as json_file:
-    modifier_data = json.load(json_file)
+    mod_list = json.load(json_file)
 
-mod_list = {"Strength": modifier_data['str'], "Intelligence": modifier_data['int'], "Wisdom": modifier_data['wis'],
-            "Dexterity": modifier_data['dex'], "Constitution": modifier_data['con'], "Charisma": modifier_data['cha']}
+with open('data/classes.json') as json_class_file:
+    class_data = json.load(json_class_file)
 
-class_list = ["Fighter", "Magic-User", "Cleric", "Thief", "Dwarf", "Halfling", "Elf"]
+class_list = [class_name for class_name in class_data.keys()]
 recommended_classes = {}
 
 
@@ -17,7 +18,7 @@ def main():
     ability_dict = {}
     ability_scores = {}
     headings = ['Attribute', 'Score', 'Dice Rolls', 'Modifier', '', 'Class', 'Bonus']
-    attributes = ["Strength", "Intelligence", "Wisdom", "Dexterity", "Constitution", "Charisma"]
+    attributes = [att for att in mod_list.keys()]
     scores = []
     dice_rolls = []
     modifiers = []
@@ -35,10 +36,7 @@ def main():
 
     bonuses = []
     for class_name in class_list:
-        if class_name in ["Halfling", "Elf"]:
-            bonuses.append(get_two_prime_stat_bonus(class_name, ability_scores))
-        else:
-            bonuses.append(get_one_prime_stat_bonus(class_name, ability_scores))
+        bonuses.append(calc_stat_bonus(class_name, ability_scores, class_data))
 
     gold_roll = dice.roll('3d6')
 
@@ -57,6 +55,10 @@ def main():
     s = tab.draw()
     print(s)
 
+    recommend_classes(scores)
+
+
+def recommend_classes(scores):
     bad_rolls = 0
     good_rolls = 0
     for score in scores[:-1]:
@@ -64,61 +66,42 @@ def main():
             bad_rolls += 1
         if score > 9:
             good_rolls += 1
-
     if bad_rolls >= 2:
         print("!!!!!!!!! More than 1 skill 6 or lower, Recommended ReRoll !!!!!!!!!")
-
     if good_rolls == 0:
         print("!!!!!!!!! No good skills, Recommended ReRoll !!!!!!!!!")
-
     if len([v for v in [1, 2] if v in recommended_classes.keys()]) <= 0:
         print("\nNo class has any bonuses. Maybe go with Magic-User or Fighter?\n")
-
     if 2 in recommended_classes.keys():
         print("STRONGLY RECOMMENDED CLASSES: " + ", ".join(recommended_classes[2]))
-
     if 1 in recommended_classes.keys():
         print("Recommended Classes: " + ", ".join(recommended_classes[1]))
 
 
-def get_one_prime_stat_bonus(class_name, ability_scores):
-    class_prime_req = {"Fighter": "Strength", "Magic-User": "Intelligence", "Cleric": "Wisdom", "Thief": "Dexterity",
-                       "Dwarf": "Strength"}
+def calc_stat_bonus(class_name, ability_scores, class_data):
+    too_low_att = []
 
-    if class_name == 'Dwarf' and ability_scores['Constitution'] < 9:
-        return "Constitution too low to be Dwarf"
+    for prime_req, req_value in class_data[class_name]["Requirements"].items():
+        if ability_scores[prime_req] < req_value:
+            too_low_att.append(prime_req)
 
-    if ability_scores[class_prime_req[class_name]] >= 16:
-        recommended_classes.setdefault(2, []).append(class_name)
-        return "\t+{bonus}% XP".format(bonus="10")
-
-    if ability_scores[class_prime_req[class_name]] >= 13:
-        recommended_classes.setdefault(1, []).append(class_name)
-        return "\t+{bonus}% XP".format(bonus="5")
-
-    if ability_scores[class_prime_req[class_name]] <= 8:
-        return "\t-{bonus}% XP".format(bonus="10" if ability_scores[class_prime_req[class_name]] <= 5 else "5")
-
-    else:
-        return "No bonus to XP"
-
-
-def get_two_prime_stat_bonus(class_name, ability_scores):
-    class_prime_req = {"Elf": ["Strength", "Intelligence"],
-                       "Halfling": ["Strength", "Dexterity"]}
-
-    if class_name == 'Elf' and ability_scores['Intelligence'] < 9:
-        return "Intelligence too low to be Elf"
-
-    if class_name == 'Halfling' and (ability_scores['Dexterity'] < 9 or ability_scores['Constitution'] < 9):
-        return "Dexterity / Constitution too low to be Halfling"
+    if len(too_low_att) > 0:
+        return "{0} too low to be {1}".format(", ".join(too_low_att), class_name)
 
     bonuses = 0
-    for att in class_prime_req[class_name]:
+    if len(class_data[class_name]["Prime-Requisite"]) == 2:
+        for att in class_data[class_name]["Prime-Requisite"]:
+            if ability_scores[att] >= 13:
+                bonuses += 1
+            if ability_scores[att] <= 8:
+                bonuses -= 1
+
+    if len(class_data[class_name]["Prime-Requisite"]) == 1:
+        att = class_data[class_name]["Prime-Requisite"][0]
         if ability_scores[att] >= 13:
-            bonuses += 1
+            bonuses = 2 if ability_scores[att] >= 16 else 1
         if ability_scores[att] <= 8:
-            bonuses -= 1
+            bonuses = -2 if ability_scores[att] <= 5 else -1
 
     if bonuses != 0:
         recommended_classes.setdefault(bonuses, []).append(class_name)
